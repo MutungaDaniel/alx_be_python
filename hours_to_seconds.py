f8f4861b8bb4693677884e12e75e11d0f70787c6
MutungaDaniel <mutungamusyoka28@gmail.com>
2025-07-13 07:32:03 +0300

    I added more files to my repository

---
+++ hours_to_seconds.py
@@ -0,0 +1,8 @@
+#Lets define and assign our variables
+hours = 2
+
+#Lets do our calculations now
+seconds = hours * 3600
+
+#Time to print our result
+print(hours, "hour(s) is ", seconds, "seconds.")
---
+++ rectangle_area.py
@@ -0,0 +1,9 @@
+#Lets start by defining the variables
+length = 10
+width = 5
+
+#Calculations
+area = length * width
+
+#Time to print out the results
+print("The area of the rectangle is: ", area)
